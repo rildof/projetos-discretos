1c4fe9cec11ca6920cbec7e744789ba1e648872e
Rildo <rildo.barbosafilho@ufpe.br>
2023-12-12 22:20:44 -0300

    add if statements

--- cliente.py
+++ cliente.py
@@ -9,25 +9,31 @@ class Cliente(RSAsystem):
     def __init__(self, nome):
         p, q = get_primes()
         super().__init__(p, q)
-        self.comunicador = MQTTCommunicator(nome, 'localhost')
+        self.comunicador = MQTTCommunicator(nome, 'broker.hivemq.com')
         self.clients = {}
-        self.currentPK = 0
+        self.current_e = 0
+        self.current_n = 0
 
 logging.basicConfig(level=logging.INFO)
 
-cliente1 = Cliente('alice')
+cliente1 = Cliente('bob')
 
 cliente1.comunicador.start()
 cliente1.comunicador.subscribe('amigos')
 
 while True:
-    if keyboard.is_pressed('enter'):
+    if keyboard.is_pressed('/'):
         time.sleep(0.5)
         client_input = input()
-        cliente1.comunicador.publish('amigos', client_input)
+        if client_input == 'publickey':
+            cliente1.comunicador.publish('amigos', 'publickey' + ' ' + str(cliente1.e) + ' ' + str(cliente1.n))
+        else:
+            cliente1.comunicador.publish('amigos', client_input)
         time.sleep(0.5)
 
     if 'publickey' in cliente1.comunicador.info:
-        cliente1.currentPK = cliente1.comunicador.info.split(' ')[1]
-    print(cliente1.currentPK)
+        e_sender, n_sender = cliente1.comunicador.info.split(' ')[1], cliente1.comunicador.info.split(' ')[2]
+        if(e_sender != str(cliente1.e) or n_sender != str(cliente1.n)):
+            cliente1.current_e, cliente1.current_n = e_sender, n_sender
+    print(cliente1.current_e, cliente1.current_n)
 
